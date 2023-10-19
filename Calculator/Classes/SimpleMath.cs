9892db05fdce1ffa49da3bc7b1fab789e35a44bf
totarochristian <totaro.christian.18@gmail.com>
2023-10-19 16:54:56 +0200

    Add comments to explain methods of simple math class

--- Calculator/Classes/SimpleMath.cs
+++ Calculator/Classes/SimpleMath.cs
@@ -8,23 +8,50 @@ using System.Windows;
 
 namespace Calculator.Classes
 {
+    /// <summary>
+    /// Class that give some baic math operations.
+    /// </summary>
     public static class SimpleMath
     {
+        /// <summary>
+        /// Method used to add two numbers
+        /// </summary>
+        /// <param name="n1">Number 1 as double</param>
+        /// <param name="n2">Number 2 as double</param>
+        /// <returns>Double that is the result of the operation executed</returns>
         public static double Addition(double n1, double n2)
         {
             return n1 + n2;
         }
 
+        /// <summary>
+        /// Method used to subtract two numbers
+        /// </summary>
+        /// <param name="n1">Number 1 as double</param>
+        /// <param name="n2">Number 2 as double</param>
+        /// <returns>Double that is the result of the operation executed</returns>
         public static double Subtraction(double n1, double n2)
         {
             return n1 - n2;
         }
 
+        /// <summary>
+        /// Method used to multiply two numbers
+        /// </summary>
+        /// <param name="n1">Number 1 as double</param>
+        /// <param name="n2">Number 2 as double</param>
+        /// <returns>Double that is the result of the operation executed</returns>
         public static double Multiply(double n1, double n2)
         {
             return n1 * n2;
         }
 
+        /// <summary>
+        /// Method used to divide two numbers
+        /// </summary>
+        /// <param name="n1">Number 1 as double</param>
+        /// <param name="n2">Number 2 as double</param>
+        /// <returns>Double that is the result of the operation executed</returns>
         public static double Divide(double n1, double n2)
         {
             //If second number is 0, show error and return 0 as result
