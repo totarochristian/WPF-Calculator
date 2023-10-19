80c69ee2e1a8e61c5e71ffd879f5fc40a4a14a17
totarochristian <totaro.christian.18@gmail.com>
2023-10-19 16:52:32 +0200

    Update divide method in SimpleMath, if second number is 0, show error and return 0 as result

--- Calculator/Classes/SimpleMath.cs
+++ Calculator/Classes/SimpleMath.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace Calculator.Classes
 {
@@ -26,6 +27,13 @@ namespace Calculator.Classes
 
         public static double Divide(double n1, double n2)
         {
+            //If second number is 0, show error and return 0 as result
+            if (n2 == 0)
+            {
+                MessageBox.Show("Division by 0 is not supported!", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
+                
             return n1 / n2;
         }
     }
